d56695bfa116880f07b9a933183163d6e1d0c2bc
Chanakya Devendra <101084514+mrdev-19@users.noreply.github.com>
2024-05-18 21:21:09 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import cv2
+import pandas as pd
+from tempfile import NamedTemporaryFile
+#---------------------------------------------------
+
+page_title="MyWays.ai Results"
+page_icon=""
+layout="centered"
+
+st.set_page_config(page_title=page_title,page_icon=page_icon,layout=layout)
+st.title(page_title+" "+page_icon)
+st.header("Results ledu Bongu Ledu")
+st.header("Fukk U !!")
+st.header("Call me and Apologize now 😤😤😤")
+st.image("fu.webp")
